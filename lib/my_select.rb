6a212aace563fd01b473571a0e234d481157518a
jameseclement <jameseclement@gmail.com>
2018-12-28 00:02:09 +0000

    Done.

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,11 +1,12 @@
 def my_select(collection)
 i = 0
 array = []
-while i < array.length
+while i < collection.length
 
-if yield(array[i]) == true 
-  array << yield(array[i])
-end
-i +=1 
+if yield(collection[i]) == true 
+  array << (collection[i])
 end 
+i += 1
+end
+array
 end
